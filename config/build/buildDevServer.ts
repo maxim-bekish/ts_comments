6ac3fe68b6fb18b2e391b10ad882b5abf37292a6
Maksim Bekis <maxamax997@gmail.com>
2024-01-31 09:46:48 +0300

    restart

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,9 @@
+import { BuildOptions } from "./types/types";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+
+export function buildDevServer(options: BuildOptions): DevServerConfiguration {
+  return {
+    port: options.port ?? 3000,
+    open: true,
+  };
+}
---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,39 @@
+import { ModuleOptions } from "webpack";
+import { BuildOptions } from "./types/types";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export function buildLoaders(options: BuildOptions): ModuleOptions["rules"] {
+  const isDev = options.mode === "development";
+  const scssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      // Translates CSS into CommonJS
+      "css-loader",
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  };
+  const tsLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+  const assetLoader = {
+    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+   
+    type: "asset/resource",
+  };
+  const test = {
+    test: /\.html$/i,
+    loader: "html-loader",
+  };
+
+
+  return [test,assetLoader,scssLoader, tsLoader];
+}
+
+
+
+      
